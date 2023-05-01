7540e5138416ac868bca573fa6658aa177c4f3bc
AdrianGolea <adyx102001@gmail.com>
2023-05-01 12:41:37 +0300

    Added a ClassifiedFile class, the PoliceInspectorateContext, a Repository for ClassifiedFiles, updated a reference in BaseRepository and added the IClassifiedFilesRepository interface

---
+++ SE_PoliceInspectorate.DataAccess.EF/PoliceInspectorateContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SE_PoliceInspectorate.DataAccess.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace SE_PoliceInspectorate.DataAccess.EF
+{
+    public class PoliceInspectorateContext : IdentityDbContext<User, Role, int>
+    {
+        public PoliceInspectorateContext(DbContextOptions<PoliceInspectorateContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ClassifiedFile> ClassifiedFile { get; set; }
+    }
+}
+
+
+
+
+
+
