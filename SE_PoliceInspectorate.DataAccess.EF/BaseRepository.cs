d80d5de5394fd26f77ac275e2e3458503e9f6c57
Julia Florea <julia.florea5@outlook.com>
2023-05-01 12:15:31 +0300

    created classes

---
+++ SE_PoliceInspectorate.DataAccess.EF/BaseRepository.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using global::PoliceInspectorate.DataAccess.Model;
+using SE_PoliceInspectorate.DataAccess.Abstractions;
+using System.Data.Entity;
+using SE_PoliceInspectorate.DataAccess.Abstraction;
+using SE_PoliceInspectorate.DataAccess.Model;
+
+namespace SE_PoliceInspectorate.DataAccess.EF
+{
+    public class BaseRepository<T> : IBaseRepository<T> where T : class, IEntity
+    {
+        protected readonly PoliceInspectorateContext dbContext;
+        public BaseRepository(PoliceInspectorateContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public T Add(T element)
+        {
+            var returnEntity = dbContext.Set<T>()
+                                        .Add(element)
+                                     .Entity;
+
+            return returnEntity;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var itemToRemove = await GetByIdAsync(id);
+            if (itemToRemove != null)
+            {
+                dbContext.Set<T>().Remove(itemToRemove);
+                return true;
+            }
+            return false;
+        }
+
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return dbContext.Set<T>().AsNoTracking();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await GetAll()
+                             .FirstAsync(entity => entity.Id == id);
+        }
+
+        public virtual T Update(T elementToUpdate)
+        {
+            var returnEntity = dbContext.Set<T>()
+                                        .Update(elementToUpdate)
+                                     .Entity;
+
+            return returnEntity;
+        }
+
+        public async Task SaveAsync()
+        {
+            await dbContext.SaveChangesAsync();
+        }
+    }
+
+}
---
+++ SE_PoliceInspectorate.DataAccess.Model/User.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SE_PoliceInspectorate.DataAccess.Model;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace SE_PoliceInspectorate.DataAccess.Model
+{
+    public class User : IdentityUser<int>, IEntity
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+
+        public int? PoliceStationId { get; set; }
+        public PoliceStation? PoliceStation { get; set; }
+
+        [InverseProperty("From")]
+        public ICollection<ConferenceMessage> SentMessages { get; set; }
+        [InverseProperty("To")]
+        public ICollection<ConferenceMessage> ReceivedMessages { get; set; }
+        [InverseProperty("CreatedBy")]
+        public ICollection<ClassifiedFile> CreatedFiles { get; set; }
+        [InverseProperty("UpdatedBy")]
+        public ICollection<ClassifiedFile> UpdatedFiles { get; set; }
+
+
+    }
+}
+
