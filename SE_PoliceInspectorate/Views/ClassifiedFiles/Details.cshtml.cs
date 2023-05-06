fc735e76079a58f5357e3c3ce25a14049102833d
AdrianGolea <adyx102001@gmail.com>
2023-05-06 12:41:26 +0300

    Added a ClassifiedFiles Controller with the required views

---
+++ SE_PoliceInspectorate/Views/ClassifiedFiles/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SE_PoliceInspectorate.Views.ClassifiedFiles
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
