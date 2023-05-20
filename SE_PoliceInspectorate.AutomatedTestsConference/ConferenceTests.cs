82a074b2227c49bd7b0cc2c6c481622270016616
vlad <vlad@DESKTOP-SADLB8C>
2023-05-20 12:31:53 +0300

    Am facut niste automated tests pt Conference.

---
+++ SE_PoliceInspectorate.AutomatedTestsConference/ConferenceTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ConferenceMessageTests
+{
+    [TestClass]
+    public class ConferenceMessageTests
+    {
+        private IWebDriver driver;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("http://localhost:port/ConferenceMessage/Index");
+        }
+
+        [TestMethod]
+        public void VerifyIndexPageTitle()
+        {
+            string expectedTitle = "Conference Message Index";
+            string actualTitle = driver.Title;
+
+            Assert.AreEqual(expectedTitle, actualTitle);
+        }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
